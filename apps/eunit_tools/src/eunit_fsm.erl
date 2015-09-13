2e50acf353c0e88c794ddcd41e313927b0523742
vlad <vlad@maveric.acassa>
2015-09-13 23:38:25 +0300

    dots

--- apps/eunit_tools/src/eunit_fsm.erl
+++ apps/eunit_tools/src/eunit_fsm.erl
@@ -17,7 +17,7 @@ get_status(Pid, Which) ->
 translateCmd(Id, {state, is, X}) ->
     case get_status(Id, "StateName") of
 	X -> true;
-	V -> .erlang:error({state_match_failed,
+	V -> erlang:error({state_match_failed,
 			    [{module, ?MODULE},
 			     {line, ?LINE},
 			     {expected, X},
@@ -27,7 +27,7 @@ translateCmd(_Id, {call, M, F, A, X}) ->
 %%    io:format("~p:~p~n", [M, F]),
     case apply(M, F, A) of
 	X -> ok;
-	V -> .erlang:error({function_call_match_failed,
+	V -> erlang:error({function_call_match_failed,
 			    [{module, ?MODULE},
 			     {line, ?LINE},
 			     {expression, ?Expr(apply(M, F, A))},
@@ -37,7 +37,7 @@ translateCmd(_Id, {call, M, F, A, X}) ->
 translateCmd(Id, {loopdata, is, X}) ->
     case tl(tuple_to_list(get_status(Id, "StateData"))) of
 	X -> true;
-	V -> .erlang:error({loopdata_match_failed,
+	V -> erlang:error({loopdata_match_failed,
 			    [{module, ?MODULE},
 			     {line, ?LINE},
 			     {expected, X},
